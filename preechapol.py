384e5976e3f816bb1cd0823bfe2df06ea0bfd109
preechapol <93781896+preechapol@users.noreply.github.com>
2021-11-05 22:21:55 +0700

    Update preechapol.py

--- preechapol.py
+++ preechapol.py
@@ -4,7 +4,7 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('preechapol', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(5) # 5hz
     while not rospy.is_shutdown():
